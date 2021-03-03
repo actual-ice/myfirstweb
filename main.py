5182f98aa86e4aa053e78dd21c976aa2d3176d3f
actual-ice <isaiah.penullar@gmail.com>
2021-03-03 15:26:07 +0800

    added comments to main.py

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from flask import Flask, redirect, url_for, render_template, request, session
 
+#initialize the app
 app = Flask(__name__)
 app.secret_key = "fensNKJB231&^**(6JHBbfhe&%"
 
+
 @app.route("/", methods=["POST", "GET"])
 def home():
+    """
+    if the request was from posting a form, check the input, and if input is "gay"
+    then redirect to "enter"
+    """
     if request.method == "POST":
         if request.form["passwd"] == "gay":
             passwd = request.form["passwd"]
@@ -21,10 +27,16 @@ def home():
 
 @app.route("/correct")
 def enter():
+    """
+    if user entered correct password from "home" then render "congrats" on screen
+    if user attemps to cheat and types /correct without entering correct password, redirect user
+    to rick atsley
+    """
     if not session:
         return redirect("https://www.youtube.com/watch?v=dQw4w9WgXcQ")
     elif session["passwd"] == "gay":
         return "<h1>congrats</h1>"
 
+#driver functions
 if __name__ == "__main__":
     app.run(debug=True)
